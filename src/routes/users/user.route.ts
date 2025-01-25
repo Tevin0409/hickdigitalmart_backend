0ecda3529cc5728acc1fb84e2b4af4a3b767c1a1
Ian Njoroge <iannjoroge@Macs-MacBook-Pro-3.local>
2025-01-25 09:09:43 +0300

    added user role

--- src/routes/users/user.route.ts
+++ src/routes/users/user.route.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { userController } from "../../controller";
+import { userController ,roleController} from "../../controller";
 
 const userRouter = express.Router();
 
@@ -9,5 +9,6 @@ userRouter.put("/update-user/:id", userController.updateUser);
 userRouter.get("/get-all-users", userController.getAllUsers);
 userRouter.get("/get-user/:id", userController.getUserById);
 userRouter.get("/get-user-by-email/:email", userController.getUserByEmail);
+userRouter.get("/roles", roleController.getAllRoles);
 
 export { userRouter };
