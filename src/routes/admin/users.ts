a33a086710045845b369d51e9341811ccc3b27f9
Ian Njoroge <iannjoroge@Macs-MacBook-Pro-3.local>
2025-01-26 09:04:39 +0300

    added auth

--- src/routes/admin/users.ts
+++ src/routes/admin/users.ts
@@ -1,13 +1,30 @@
 import express from "express";
 import { userController } from "../../controller";
+import { authMiddleware } from "../../middleware";
 
 const userAdminRouter = express.Router();
 
-userAdminRouter.post("/create-user", userController.createUser);
+userAdminRouter.post("/create-user", authMiddleware, userController.createUser);
 userAdminRouter.post("/login", userController.login);
-userAdminRouter.put("/update-user/:id", userController.updateUser);
-userAdminRouter.get("/get-all-users", userController.getAllUsers);
-userAdminRouter.get("/get-user/:id", userController.getUserById);
-userAdminRouter.get("/get-user-by-email/:email", userController.getUserByEmail);
+userAdminRouter.put(
+  "/update-user/:id",
+  authMiddleware,
+  userController.updateUser
+);
+userAdminRouter.get(
+  "/get-all-users",
+  authMiddleware,
+  userController.getAllUsers
+);
+userAdminRouter.get(
+  "/get-user/:id",
+  authMiddleware,
+  userController.getUserById
+);
+userAdminRouter.get(
+  "/get-user-by-email/:email",
+  authMiddleware,
+  userController.getUserByEmail
+);
 
 export { userAdminRouter };
